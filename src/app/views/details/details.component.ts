25ee86fb559fad36135515ecd2b09c7a722267ac
jramirez <jose.ramirez@siigroup.cl>
2021-06-15 14:01:26 -0400

    feat(details.component): adding view and logic to details.component

--- src/app/views/details/details.component.ts
+++ src/app/views/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { timer } from 'rxjs';
+import { IndicatorHistoricInterface, IndicatorHistoricItemInterface } from 'src/app/shared/interfaces/indicator-historic.interface';
 import { IndicatorsService } from 'src/app/shared/services/indicators.service';
 
 @Component({
@@ -9,26 +11,67 @@ import { IndicatorsService } from 'src/app/shared/services/indicators.service';
 })
 export class DetailsComponent implements OnInit {
 
-  indicator = '';
+  indicator: {
+    codigo: string,
+    fecha: string,
+    nombre: string,
+    unidad_medida: string,
+    valor: number;
+  } = {
+    codigo: "",
+    fecha: "",
+    nombre: "",
+    unidad_medida: "",
+    valor: 0
+  };
+  indicatorType = '';
   isLoading = true;
 
   constructor(
+    private router: Router,
     private activatedRoute: ActivatedRoute,
     private indicatorsService: IndicatorsService
   ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
-      this.indicator = params.indicator
-      console.log(this.indicator)
-      this.getIndicatorData(this.indicator);
+      this.indicatorType = params.indicator
+      console.log(this.indicatorType)
+      this.getIndicatorData(this.indicatorType);
     })
   }
 
   getIndicatorData(type: string) {
+
+    if (typeof(Storage) !== 'undefined') {
+      let indicatorsRawData = sessionStorage.getItem('indicators');
+      if (indicatorsRawData !== null) {
+        indicatorsRawData = JSON.parse(indicatorsRawData)
+        this.indicator = Object.entries(indicatorsRawData !== null && indicatorsRawData).filter((el: any) => {
+          if (el[0] === this.indicatorType) {
+            return el;
+          }
+        })[0][1];
+        console.log(this.indicator)
+        const timerLoad = timer(600);
+        timerLoad.subscribe(() => this.isLoading = false);
+      }
+    } else {
+      this.getIndicatorDataFromService(type);
+    }
+
+  }
+
+  getIndicatorDataFromService(type: string) {
     this.indicatorsService.getIndicatorByType(type).subscribe(data => {
       console.log(data);
+      const timerLoad = timer(600);
+      timerLoad.subscribe(() => this.isLoading = false);
     })
+  }
+
+  goTo() {
+    this.router.navigate([''])
   }
 
 
